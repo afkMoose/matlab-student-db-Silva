02eca682179a5c50af8f9454c0086194100ab317
afkMoose <silvam76@rowan.edu>
2024-11-15 18:02:01 -0500

    main.m

--- midterm/main.m
+++ midterm/main.m
@@ -5,6 +5,12 @@ db = StudentDatabase();  % Create a new StudentDatabase object
 db = db.addStudent(Student(1, 'Matthew', 18, 3.2, 'Mechanical Engineering'));
 db = db.addStudent(Student(2, 'Sam', 21, 3.5, 'Chemistry'));
 db = db.addStudent(Student(3, 'Voldemort', 19, 3.9, 'Wizardry'));
+db = db.addStudent(Student(4, 'Sarah', 18, 3.2, 'Biology'));
+db = db.addStudent(Student(5, 'Anthony', 20, 3.8, 'Chemistry'));
+db = db.addStudent(Student(6, 'Jack', 18, 3.9,'Mechanical Engineering'));
+db = db.addStudent(Student(7, 'Daniels', 19, 3.6, 'Mechanical Engineering'));
+db = db.addStudent(Student(8, 'Harry', 21, 3.5, 'Business'));
+db = db.addStudent(Student(9, 'Bob', 19, 3.3, 'Finance'));
 
 % Performing searches
 student = db.findStudentByID(1);  % Find student by ID
